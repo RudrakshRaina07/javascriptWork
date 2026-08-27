d342057b11ee4e5180a0d082276144f252adc45b
Rudraksh Raina <1819rudraksh@gmail.com>
2026-08-27 19:17:38 +0530

    setup and datatypes basics

---
+++ 01_Basics/01-dataTypes.js
@@ -0,0 +1,30 @@
+let a = null
+console.log(typeof a) //object
+
+console.log(undefined) //undefined
+
+let arr = ["mannat", "uddhav", "avnoor"]
+
+let myObj = {
+    name: "Rudraksh",
+    age: 21
+}
+
+const myFunc = function() {
+    "hellow"
+}
+
+console.log(typeof arr) //object
+console.log(typeof myObj) //object
+console.log(typeof myFunc) //function object
+
+// PRIMITIVE : 
+// STRING, NUMBER, BOOLEAN, null, undefined, bigInt, Symbol
+
+// NON-PRIMITIVE (REFERENCE)
+// Object, Arrays, Function
+
+let str = ""
+console.log("");
+
+ 
