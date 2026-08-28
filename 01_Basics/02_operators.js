2ff431452bdc6d5426663ab6ed1d934e956cab2b
Rudraksh Raina <1819rudraksh@gmail.com>
2026-08-28 18:05:55 +0530

    baisc of strings and increment and decrement

---
+++ 01_Basics/02_operators.js
@@ -0,0 +1,16 @@
+// let x = 9
+
+// console.log(x)
+
+// console.log(++x)
+// console.log(x)
+// console.log(x++)
+// console.log(x)
+
+let y = 5
+
+console.log(y)
+console.log(--y)
+console.log(y)
+console.log(y--)
+console.log(y)
---
+++ 01_Basics/03_strings.js
@@ -0,0 +1,16 @@
+let gameName = new String("rudraksh-rudr-07")
+console.log(gameName)
+
+const name = "rudraksh";
+
+console.log(`My name is ${name}`)
+
+console.log(gameName.concat(" ",name," ", gameName))
+
+console.log(gameName.replace("rudr-", "mann"))
+console.log(gameName.split())
+console.log(gameName.split("-"))
+console.log(gameName.split("-", 1))
+
+console.log(gameName.toString())
+console.log(gameName.valueOf())
