8d4645d3e6d1ec65046493602ff55a5fc0511da3
Rudraksh Raina <1819rudraksh@gmail.com>
2026-08-28 18:43:42 +0530

    nums and math in js

---
+++ 01_Basics/04_nums_and_math.js
@@ -0,0 +1,29 @@
+const number = new Number(1000)
+
+// console.log(number)
+
+// console.log(number.toExponential())  1e+3
+
+// console.log(number.toFixed(2)) 
+
+const newNum = new Number(10000000)
+
+// console.log(newNum.toLocaleString())
+// console.log(newNum.toLocaleString("en-IN"))
+
+// const num = 134.654
+
+// console.log(num.toPrecision(4))
+
+// console.log(number.toString())
+
+// console.log(number.valueOf())
+
+// ++++++++++++ Math +++++++++++
+
+console.log(Math.floor(Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
